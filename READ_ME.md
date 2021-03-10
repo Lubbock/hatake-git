29f730a47c200fdab5b97c1d072a80cf24b1df1e
guoxj <guoxj@koal.com>
2021-03-10 17:49:15 +0800

    feature: 初始化项目添加
    初始化项目添加

---
+++ READ_ME.md
@@ -0,0 +1,3 @@
+# hatake-git
+
+`Git`的java版本实现，实现原理依据文章:[源码解析：Git的第一个提交是什么样的](https://zhuanlan.zhihu.com/p/257084586)
---
+++ src/main/java/com/lame/hatake/hgit/Hgit.java
@@ -0,0 +1,7 @@
+package com.lame.hatake.hgit;
+
+public class Hgit {
+    public static void main(String[] args) {
+        System.out.println("For Seven");
+    }
+}
