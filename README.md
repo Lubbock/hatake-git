ca3d7138c4d7c3c13763d42df1011d5c687bde24
guoxj <guoxj@koal.com>
2021-03-16 10:59:45 +0800

    doc: 文档变更提交
    文档变更提交

---
+++ README.md
@@ -0,0 +1,8 @@
+# hatake-git
+
+`Git`的java版本实现，实现原理依据文章:[源码解析：Git的第一个提交是什么样的](https://zhuanlan.zhihu.com/p/257084586)
+
+[toc]
+
+## index文件
+ `index`中，文件变更记录uid,ino改为8byte
--- src/main/java/com/lame/hatake/hgit/entry/ModifierFile.java
+++ src/main/java/com/lame/hatake/hgit/entry/ModifierFile.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class ModifierFile {
     private long ctime; //创建时间
     private long mtime;//修改时间
-    private Long dev;//开发分支
-    private Long ino;//提交分支
+    private long dev;//开发分支
+    private long ino;//提交分支
     private int mode;// 提交模式 0 修改 1 增加 2 删除
     private int uid ;
     private int gid;
