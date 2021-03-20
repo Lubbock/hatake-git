a21b8f60834fbd35c02e9bbcbb477508dd652cd0
ayer <Ayer1994@outlook.com>
2021-03-20 23:16:44 +0800

    feature: 增加commit对象的关键描述
    增加commit对象的关键描述

---
+++ src/main/java/com/lame/hatake/hgit/CommitTree.java
@@ -0,0 +1,17 @@
+package com.lame.hatake.hgit;
+
+/**
+ * commit 对象,parent.n 指向了所有的commit 和 sha1
+ * */
+public class CommitTree {
+
+    public static void commitTree(String treeName, String lastCommit) {
+        //后续仿照writeTree就可以完成了，这里就不做处理了
+    }
+
+    public static void main(String[] args) {
+        //传入参数
+        //最新的commit
+        //要提交的tree
+    }
+}
